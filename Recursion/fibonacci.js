9ccbad69581af82a26b4a9532c652da1e8e9f036
Lucas-Masaba <masabaluke@gmail.com>
2023-03-07 17:45:01 +0300

    Configure typescript

--- Recursion/fibonacci.js
+++
@@ -1,6 +0,0 @@
-const fib = (n) => {
-  if(n <= 2) return 1
-  return fib(n-1) + fib(n-2)
-}
-
-fib(5)
---
+++ Recursion/fibonacci.ts
@@ -0,0 +1,6 @@
+const fib = (n: number): number => {
+  if (n <= 2) return 1
+  return fib(n - 1) + fib(n - 2)
+}
+
+console.log(fib(5))
