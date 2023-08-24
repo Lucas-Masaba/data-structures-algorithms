227859300743ee63ae2f6778f327b11aeaac2320
Lucas-Masaba <masabaluke@gmail.com>
2023-08-24 20:40:24 +0300

    Solve uncommon from sentences problem from leetcode

--- challenges/frequency_counter/frequencyCounter.js
+++ challenges/frequency_counter/frequencyCounter.js
@@ -1,15 +1,15 @@
-function sameFrequency(num1,num2){
+function sameFrequency(num1, num2) {
   let numStr1 = num1.toString()
   let numStr2 = num2.toString()
   if (numStr1.length !== numStr2.length) return false;
   let obj = {}
-  for(let i = 0; i < numStr1.length; i++){
+  for (let i = 0; i < numStr1.length; i++) {
     obj[numStr1[i]] = (obj[numStr1[i]] || 0) + 1
     // console.log(obj[numStr1[i]])
   }
 
-  for(let i = 0; i < numStr2.length; i++){
-    if(!obj[numStr2[i]]){
+  for (let i = 0; i < numStr2.length; i++) {
+    if (!obj[numStr2[i]]) {
       return false
     }
     obj[numStr2[i]]--
