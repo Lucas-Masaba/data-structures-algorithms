397b1b7b67a35e471e81ceab82c423afa4112bc6
Lucas-Masaba <masabaluke@gmail.com>
2023-09-26 16:00:54 +0300

    Change frequency counter challenge to typescript

--- challenges/frequency_counter/frequencyCounter.ts
+++ challenges/frequency_counter/frequencyCounter.ts
@@ -1,8 +1,13 @@
-function sameFrequency(num1, num2) {
+function sameFrequency(num1: number, num2: number) {
   let numStr1 = num1.toString()
   let numStr2 = num2.toString()
   if (numStr1.length !== numStr2.length) return false;
-  let obj = {}
+
+  interface objInterface { 
+    [key: string]: number
+  }
+
+  let obj:objInterface = {}
   for (let i = 0; i < numStr1.length; i++) {
     obj[numStr1[i]] = (obj[numStr1[i]] || 0) + 1
     // console.log(obj[numStr1[i]])
