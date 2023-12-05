a5ab552b55264b05e3e8cd25694a7662190edc32
Lucas-Masaba <masabaluke@gmail.com>
2023-12-05 23:03:09 +0300

    Change file names

--- Recursion/reverse.js
+++
@@ -1,8 +0,0 @@
-function reverse(str){
-  console.log(str)
-	if(str.length <= 1) return str;
-	return reverse(str.slice(1)) + str[0];
-}
-
-console.log(reverse('awesome')) // 'emosewa'
-// reverse('rithmschool') // 'loohcsmhtir'
